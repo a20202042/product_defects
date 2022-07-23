75fb034e4d54102b018a9b3a6cc83fcfdfd2661c
PDAL_HU <a20202042@gmail.com>
2022-07-23 16:49:49 +0800

    主程式修改

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import cv2
 import numpy as np
-import math, imutils
+import math, imutils, json, os
+from math import copysign, log10
+from sentence_transformers import SentenceTransformer, util
+from PIL import Image
+import glob
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
@@ -79,15 +83,12 @@ def angle_calculate(box):
 
 def rotate_img(img, angle):
     rotated_img = imutils.rotate_bound(img, angle)
+    # rotated_img
     return rotated_img
 
 
-def crop(box, crop_img):
-    left_point_x = np.min(box[:, 0])
-    right_point_x = np.max(box[:, 0])
-    top_point_y = np.min(box[:, 1])
-    bottom_point_y = np.max(box[:, 1])
-    range = 20
+def crop(left_point_x, right_point_x, top_point_y, bottom_point_y, crop_img):
+    range = 5
     x = min([left_point_x, right_point_x]) - range
     w = abs(right_point_x - left_point_x) + range * 2
     y = min([top_point_y, bottom_point_y]) - range
@@ -154,7 +155,94 @@ def line_detect(src):
     return cdstP
 
 
+def hu_moment(im):
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    _, im = cv2.threshold(im, 50, 255, cv2.THRESH_BINARY)
+    moments = cv2.moments(im)
+    huMoments = cv2.HuMoments(moments)
+    for i in range(0, 7):
+        huMoments[i] = -1 * copysign(1.0, huMoments[i]) * log10(abs(huMoments[i]))
+        huMoments[i] = round(huMoments[i][0], 3)
+    hu = []
+    for item in huMoments:
+        hu.append(float(item[0]))
+    return hu
+
+
+def load_hu_data():
+    f = open('data.json')
+    data = json.load(f)
+    # print(data)
+    return data
+
+
+def compare_hu_moment(file, hu, hu_data):
+    ran = 0.15
+    hu1, hu2, hu3 = bool, bool, bool
+    H_U = False
+    # print(hu_data[str(file)]['hu1'][0] * (1 - ran), ':%s:' % hu[0], hu_data[str(file)]['hu1'][1] * (1 + ran))
+    # print(hu_data[str(file)]['hu2'][0] * (1 - ran), ':%s:' % hu[1], hu_data[str(file)]['hu2'][1] * (1 + ran))
+    # print(hu_data[str(file)]['hu3'][0] * (1 - ran), ':%s:' % hu[2], hu_data[str(file)]['hu3'][1] * (1 + ran))
+    if hu_data[str(file)]['hu1'][0] * (1 - ran) < hu[0] < hu_data[str(file)]['hu1'][1] * (1 + ran):
+        hu1 = True
+    if hu_data[str(file)]['hu2'][0] * (1 - ran) < hu[1] < hu_data[str(file)]['hu2'][1] * (1 + ran):
+        hu2 = True
+    if hu_data[str(file)]['hu3'][0] * (1 - ran) < hu[2] < hu_data[str(file)]['hu3'][1] * (1 + ran):
+        hu3 = True
+    if hu1 is True and hu2 is True and hu3 is True:
+        H_U = True
+    return H_U
+
+
+def match_crop_img(img, x, y, w, h):
+    crop_img = img[y:y + h, x:x + w]
+    return crop_img
+
+
+def similarity(item):
+    model = SentenceTransformer('clip-ViT-B-32')
+    image_names = list(glob.glob('.\\match_data\\*.png'))  # 圓圖資料夾
+    image_names.append(item)
+    # print(image_names)
+    # print("Images:", len(image_names))
+    encoded_image = model.encode([Image.open(filepath) for filepath in image_names], batch_size=128,
+                                 convert_to_tensor=True, show_progress_bar=True)
+
+    processed_images = util.paraphrase_mining_embeddings(encoded_image)
+    NUM_SIMILAR_IMAGES = 10
+
+    # duplicates = [image for image in processed_images if image[0] >= 0.999]
+    #
+    # for score, image_id1, image_id2 in duplicates[0:NUM_SIMILAR_IMAGES]:
+    #     print("\nScore: {:.3f}%".format(score * 100))
+    #     print(image_names[image_id1])
+    #     print(image_names[image_id2])
+
+    threshold = 0.99
+    near_duplicates = [image for image in processed_images if image[0] < threshold]
+
+    source = {}
+    for score, image_id1, image_id2 in near_duplicates[0:NUM_SIMILAR_IMAGES]:
+        print("\nScore: {:.3f}%".format(score * 100))
+        print(image_names[image_id1])
+        print(image_names[image_id2])
+        if image_names[image_id2] == item:
+            source.update({image_names[image_id1]: score * 100})
+    print(source)
+    return source
+
+
+hu_data = load_hu_data()
+
+dir = 'match_data'
+files = os.listdir(dir)
+file_data = []
+for file in files:
+    template = cv2.imread(dir + '\\' + file)
+    file_data.append({'template': template, 'file': file})
+
 while (True):
+
     # 擷取影像
     ret, frame = cap.read()
     rotated_img, crop_img = frame.copy(), frame.copy()
@@ -164,37 +252,74 @@ while (True):
     # cv2.imshow('gray_res', gray_res)
 
     box_img, box_compact_img, box = box_compact(frame)
-    # cv2.imshow('box_compact_img', box_compact_img)
+    crop_img = crop(np.min(box[:, 0]), np.max(box[:, 0]), np.min(box[:, 1]), np.max(box[:, 1]), crop_img)
 
-    # -----抓出最小方形角度並旋轉
+    # # -----抓出最小方形角度並旋轉
     angle = angle_calculate(box)
-    # print(angle)
     rotated_img = rotate_img(rotated_img, angle)
-    # cv2.imshow('rotated_img', rotated_img)
+    # # cv2.imshow('rotated_img', rotated_img)
+    #
+    # # -----裁切出目標區域
+    # not_use, not_use_2, box = box_compact(rotated_img)
+    # crop_img = rotate_img(crop_img, angle)
+    # crop_img = crop(np.min(box[:, 0]), np.max(box[:, 0]), np.min(box[:, 1]), np.max(box[:, 1]), crop_img)
 
-    # -----裁切出目標區域
-    not_use, not_use_2, box = box_compact(rotated_img)
-    crop_img = rotate_img(crop_img, angle)
-    crop_img = crop(box, crop_img)
     try:
+        # cv2.imshow('crop_img', crop_img)
         cv2.imshow('crop_img', crop_img)
-        status = cv2.imwrite('match_data/or_6.png', crop_img)
+        check = True
+        # status = cv2.imwrite('match_data/or_3.png', crop_img)
     except:
         crop_img = np.zeros((500, 500, 3), dtype="uint8")
+        check = False
         # print('crop_img not crop')  # 測試用
 
-    # ------目標區域圓孔偵測
-    circle_detect_img = crop_img.copy()
-    circle_detect_img, circles_data = circle_detect(circle_detect_img)
+    # ------目標區域分類
+    if check is True:
+        for item in file_data:
+            template = item['template']
+            file = item['file']
+            h, w, l = template.shape
+            if crop_img.shape[0] > h and crop_img.shape[1] > w:
+                res = cv2.matchTemplate(crop_img, template, cv2.TM_CCORR_NORMED)
+                min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+                max_val_ncc = '{:.3f}'.format(max_val)
+                if float(max_val_ncc) > 0.96:
+                    match = True
+                    top_left = max_loc
+                    bottom_right = (top_left[0] + w, top_left[1] + h)
+                    hu = hu_moment(crop_img)
+                    if str(file) in hu_data.keys():
+                        HU = compare_hu_moment(str(file), hu, hu_data)
+                        # print(HU)
+                        if HU is True:
+                            hu_check = True
+                            cv2.putText(frame, item['file'] + max_val_ncc + 'hu_go', [100, 100], cv2.FONT_HERSHEY_SIMPLEX,
+                                        1,(255, 255, 0), 2, cv2.LINE_AA)
+                            break
+                        else:
+                            cv2.putText(frame, item['file'] + max_val_ncc, [100, 100], cv2.FONT_HERSHEY_SIMPLEX, 1,
+                                        (255, 255, 0), 2, cv2.LINE_AA)
+                            break
+                else:
+                    cv2.putText(frame, ' ', [100, 100], cv2.FONT_HERSHEY_SIMPLEX, 1,
+                                (255, 255, 0), 2, cv2.LINE_AA)
+            else:
+                cv2.putText(frame, 'not_find', [100, 100], cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2,
+                            cv2.LINE_AA)
+
+        cv2.imshow('cam', frame)
+
+    # # ------目標區域圓孔偵測
+    # circle_detect_img = crop_img.copy()
+    # circle_detect_img, circles_data = circle_detect(circle_detect_img)
     # cv2.imshow('circle_detect_img', circle_detect_img)
-
-
-    # ------目標區域線段偵測
-    line_target_img = crop_img.copy()
-    line_target_img = line_detect(line_target_img)
-    cv2.imshow('line_target_img', line_target_img)
+    #
+    # # ------目標區域線段偵測
+    # line_target_img = crop_img.copy()
+    # line_target_img = line_detect(line_target_img)
+    # cv2.imshow('line_target_img', line_target_img)
     # cv2.imshow('crop_img', crop_img)
-
     if cv2.waitKey(1) == ord('q'):
         break
 
