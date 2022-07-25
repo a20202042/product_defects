af8e3f9d130fff03cd84e3fac7ebc77a97e6ebca
PDAL_HU <a20202042@gmail.com>
2022-07-25 19:45:21 +0800

    比較完成:) 文字位置完成:)

--- compare.py
+++ compare.py
@@ -1,6 +1,15 @@
 from skimage.metrics import structural_similarity
-import cv2
+import cv2, pytesseract, re
 import numpy as np
+
+
+def text_find(img):
+    # img_name = 'match_data\\or_1.png'  # Input圖片檔名
+    # img = cv2.imread(img_name)
+    text = pytesseract.image_to_string(img, lang='eng')
+    text = re.sub(u"([^\u4e00-\u9fa5\u0030-\u0039\u0041-\u005a\u0061-\u007a])", "", text)
+    return text
+
 
 def find_text_position(img):
     # 讀取圖片
@@ -13,14 +22,14 @@ def find_text_position(img):
     # 二值化
     ret, binary = cv2.threshold(sobel, 50, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)
     # 膨脹、腐蝕
-    element1 = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 9))
-    element2 = cv2.getStructuringElement(cv2.MORPH_RECT, (24, 6))
+    element1 = cv2.getStructuringElement(cv2.MORPH_RECT, (50, 5))
+    element2 = cv2.getStructuringElement(cv2.MORPH_RECT, (24, 5))
     # 膨脹一次，讓輪廓突出
     dilation = cv2.dilate(binary, element2, iterations=1)
     # 腐蝕一次，去掉細節
     erosion = cv2.erode(dilation, element1, iterations=1)
     # 再次膨脹，讓輪廓明顯一些
-    dilation2 = cv2.dilate(erosion, element2, iterations=2)
+    dilation2 = cv2.dilate(erosion, element2, iterations=1)
     #  查詢輪廓和篩選文字區域
     region = []
     range_data = []
@@ -29,12 +38,12 @@ def find_text_position(img):
         cnt = contours[i]
         # 計算輪廓面積，並篩選掉面積小的
         area = cv2.contourArea(cnt)
-        if (area < 1000):
+        if (area < 500):
             continue
         # 找到最小的矩形
         rect = cv2.minAreaRect(cnt)
-        print("rect is: ")
-        print(rect)
+        # print("rect is: ")
+        # print(rect)
         # box是四個點的座標
         box = cv2.boxPoints(rect)
         box = np.int0(box)
@@ -42,28 +51,45 @@ def find_text_position(img):
         height = abs(box[0][1] - box[2][1])
         width = abs(box[0][0] - box[2][0])
         # 根據文字特徵，篩選那些太細的矩形，留下扁的
-        if (height > width * 1.3):
+        if (height > width * 1.5):
             continue
         region.append(box)
     copy_img = img.copy()
-    # 繪製輪廓
+    zero_shape = img.shape
+    zero_img = np.zeros(shape=zero_shape, dtype='uint8')
 
     for box in region:
         cv2.drawContours(copy_img, [box], 0, (0, 255, 0), 2)
+        cv2.drawContours(zero_img, [box], 0, (0, 255, 0), 2)
         range_data.append(cv2.boundingRect(box))
+        # print(box)
     for rect in range_data:
-        print(rect)
-    return range_data
+        range_ = 3
+        crop_img = copy_img[rect[1] - range_:rect[1] + rect[3] + range_ * 2,
+                   rect[0] - range_:rect[0] + rect[2] + range_ * 2]
+        text = text_find(crop_img)
+        # print(text)
+        # cv2.imshow(str(text), crop_img)
+
+    gray = cv2.cvtColor(zero_img, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    kernel = np.ones((5, 5), dtype=np.uint8)
+    closing = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+    contours, hierarchy = cv2.findContours(closing, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    cnt = max(contours, key=cv2.contourArea)
+    x, y, w, h = cv2.boundingRect(cnt)
+    cv2.rectangle(zero_img, (x, y), (x + w, y + h), (255, 255, 0), 1)
+    print(x, y, w, h)
+    return [x, y, w, h]
+
 
 # Load images
 before = cv2.imread('match_data\\or_1.png')
 before = cv2.resize(before, (250, 250), interpolation=cv2.INTER_AREA)
 
-
 after = cv2.imread('or_1_2.png')
 after = cv2.resize(after, (250, 250), interpolation=cv2.INTER_AREA)
 range_data = find_text_position(after)
-
 
 # Convert images to grayscale
 before_gray = cv2.cvtColor(before, cv2.COLOR_BGR2GRAY)
@@ -93,15 +119,18 @@ for c in contours:
     area = cv2.contourArea(c)
     if area > 350:
         x, y, w, h = cv2.boundingRect(c)
-        for item in range_data:
-            if x < item[0] < x+w and x < item[2] < x+w:
-                print('123')
-        cv2.rectangle(before, (x, y), (x + w, y + h), (36, 255, 12), 2)
-        cv2.rectangle(after, (x, y), (x + w, y + h), (36, 255, 12), 2)
-        cv2.rectangle(diff_box, (x, y), (x + w, y + h), (36, 255, 12), 2)
-        cv2.drawContours(mask, [c], 0, (255, 255, 255), -1)
-        cv2.drawContours(filled_after, [c], 0, (0, 255, 0), -1)
-
+        print(x, y, w, h)
+        print(range_data)
+        if range_data[0] < x < range_data[2] or range_data[0] < x + w < range_data[2]:
+            pass
+        elif x < range_data[0] < x + w or x < range_data[2] < x + w:
+            pass
+        else:
+            cv2.rectangle(before, (x, y), (x + w, y + h), (36, 255, 12), 2)
+            cv2.rectangle(after, (x, y), (x + w, y + h), (36, 255, 12), 2)
+            cv2.rectangle(diff_box, (x, y), (x + w, y + h), (36, 255, 12), 2)
+            cv2.drawContours(mask, [c], 0, (255, 255, 255), -1)
+            cv2.drawContours(filled_after, [c], 0, (0, 255, 0), -1)
 
 cv2.imshow('before', before)
 cv2.imshow('after', after)
